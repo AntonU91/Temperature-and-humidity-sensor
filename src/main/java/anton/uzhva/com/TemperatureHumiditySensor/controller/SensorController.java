a6793187a8172dc2dbb11550b60c98c58b4cbcd6
Anton Uzhva <anton.itec91@gmail.com>
2022-07-01 12:08:26 +0300

    ver 3.0 Added ability to update sensor name using validation.

--- src/main/java/anton/uzhva/com/TemperatureHumiditySensor/controller/SensorController.java
+++ src/main/java/anton/uzhva/com/TemperatureHumiditySensor/controller/SensorController.java
@@ -3,22 +3,18 @@ package anton.uzhva.com.TemperatureHumiditySensor.controller;
 import anton.uzhva.com.TemperatureHumiditySensor.dto.SensorDTO;
 import anton.uzhva.com.TemperatureHumiditySensor.model.Sensor;
 import anton.uzhva.com.TemperatureHumiditySensor.service.SensorService;
-import anton.uzhva.com.TemperatureHumiditySensor.util.SensorErrorResponse;
-import anton.uzhva.com.TemperatureHumiditySensor.util.SensorNotCreatedException;
-import anton.uzhva.com.TemperatureHumiditySensor.util.SensorNotFoundException;
-import anton.uzhva.com.TemperatureHumiditySensor.util.SensorValidator;
+import anton.uzhva.com.TemperatureHumiditySensor.util.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.lang.management.OperatingSystemMXBean;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -60,8 +56,28 @@ public class SensorController {
             sensorService.deleteSensor(sensor.get());
             return ResponseEntity.ok(HttpStatus.OK);
         }
-        throw new SensorNotFoundException("There no such sensor with id "+id);
+        throw new SensorNotFoundException("There no such sensor with id " + id);
     }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateSensorName(@PathVariable(name = "id") int id,
+                                                       @RequestBody @Valid SensorDTO sensorDTO, BindingResult bindingResult) throws SensorNotFoundException, SensorNameNotUpdetedException {
+        Optional<Sensor> sensor = sensorService.getSensorById(id);
+        if (sensor.isPresent()) {
+            if (bindingResult.hasErrors()) {
+                StringBuilder errorMsg = new StringBuilder();
+                List<FieldError> errors = bindingResult.getFieldErrors();
+                for (FieldError error : errors) {
+                    errorMsg.append(error.getField()).append("- ").append(error.getDefaultMessage()).append("\n");
+                }
+                throw new SensorNameNotUpdetedException(errorMsg.toString());
+            }
+            sensorService.updateSensorName(sensor.get(), sensorDTO.getName());
+            return ResponseEntity.ok(HttpStatus.OK);
+        }
+        throw new SensorNotFoundException("There no such sensor with id " + id);
+    }
+
 
     @ExceptionHandler
     private ResponseEntity<SensorErrorResponse> handleException(SensorNotCreatedException ex) {
@@ -70,14 +86,24 @@ public class SensorController {
     }
 
     @ExceptionHandler
-    private ResponseEntity<SensorErrorResponse> handleException (SensorNotFoundException ex) {
+    private ResponseEntity<SensorErrorResponse> handleException(SensorNotFoundException ex) {
         SensorErrorResponse response = new SensorErrorResponse(ex.getMessage(), new Date());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<SensorErrorResponse> handleException (SensorNameNotUpdetedException ex) {
+        SensorErrorResponse response = new SensorErrorResponse(ex.getMessage(), new Date());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 
     private Sensor convertToSensor(SensorDTO sensorDTO) {
         return modelMapper.map(sensorDTO, Sensor.class);
+    }
+
+    private SensorDTO convertToSensorDTO(Sensor sensor) {
+        return modelMapper.map(sensor, SensorDTO.class);
     }
 
 }
